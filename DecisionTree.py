1c2b987d8c66863142a905e7cd530ac7f274dba1
Dante Welch <dante@Dantes-MacBook-Pro-2.local>
2018-02-10 15:35:14 -0500

    calculates entropy for one feature

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,5 +1,6 @@
 from csv import reader
 from math import log
+from pprint import pprint
 
 class DecisionTree:
 
@@ -20,12 +21,38 @@ class DecisionTree:
 		negative = self.total_negative
 		total = self.total_positive + self.total_negative
 		self.total_entropy = (-positive / total) * log(positive / total, 2) - (negative / total) * log(negative / total, 2)
-		#print(log(5, 2))
 
-	def calculate_individual_entropy(self, dataset, column_num):
+	def calculate_each_entropy(self, dataset, column):
+		playoffs_by_stat = self.create_dictionary(dataset, column)
+		for key in playoffs_by_stat:
+			print(key)
+			print(playoffs_by_stat[key])
+			positive = playoffs_by_stat[key][0]
+			negative = playoffs_by_stat[key][1]
+			entropy = self.entropy_function(positive, negative)
+			print(entropy)
+		
+	def entropy_function(self, positive, negative):
+		total = positive + negative
+		if positive == 0:
+			entropy = - (negative / total) * log(negative / total, 2)
+		elif negative == 0:
+			entropy = -(positive / total) * log(positive / total, 2)
+		else:
+			entropy = -(positive / total) * log(positive / total, 2) - (negative / total) * log(negative / total, 2)
+		return entropy
+
+	def create_dictionary(self, dataset, column):
+		#first element is wins, second is losses
 		collection = {}
 		for row in dataset:
-			collection[row[column_num]] = []
+			collection[row[column]] = [0,0]
+		for row in dataset:
+			if row[-1] == '1':
+				collection[row[column]][0] += 1
+			else:
+				collection[row[column]][1] += 1
+		return collection
 
 	def calculate_gain(self, column):
 		print("calc gains")
@@ -46,6 +73,16 @@ dataset = read_csv(filename)
 decision_tree = DecisionTree()
 decision_tree.set_binary_metrics(dataset)
 decision_tree.calculate_total_entropy()
-decision_tree.calculate_individual_entropy(dataset, 0)
+# for i in range(len(dataset)):
+# 	for j in range(len(dataset[i])):
+# 		print(j)
+decision_tree.calculate_each_entropy(dataset, 0)
 
-print("alksjdhf")
+
+
+
+
+
+
+
+
